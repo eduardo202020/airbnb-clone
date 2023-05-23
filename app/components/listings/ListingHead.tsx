2329e2aed893754b5f94fa448614a8533239a470
Eduardo26 <jguevaral@uni.pe>
2023-05-23 09:53:27 -0500

    Individual Listing View

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,52 @@
+import useCountries from "@/app/hooks/useCountries";
+import { User } from "@prisma/client";
+import React from "react";
+import Heading from "../Heading";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+
+type Props = {
+  title: string;
+  imageSrc: string;
+  locationValue: string;
+  id: string;
+  currentUser?: User | null;
+};
+
+const ListingHead = ({
+  title,
+  imageSrc,
+  locationValue,
+  id,
+  currentUser,
+}: Props) => {
+  const { getByValue } = useCountries();
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div
+        className="
+        w-full h-[60vh] overflow-hidden rounded-xl relative
+      "
+      >
+        <Image
+          alt="Image"
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+        />
+
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListingHead;
---
+++ app/components/listings/ListingInfo.tsx
@@ -0,0 +1,70 @@
+import dynamic from "next/dynamic";
+import { IconType } from "react-icons";
+
+import useCountries from "@/app/hooks/useCountries";
+import { User } from "@prisma/client";
+
+import Avatar from "../Avatar";
+import ListingCategory from "./ListingCategory";
+
+const Map = dynamic(() => import("../Map"), { ssr: false });
+
+type Props = {
+  user: User;
+  category:
+    | {
+        icon: IconType;
+        label: string;
+        description: string;
+      }
+    | undefined;
+  roomCount: number;
+  guestCount: number;
+  bathroomCount: number;
+  locationValue: string;
+  description: string;
+};
+
+const ListingInfo = ({
+  user,
+  category,
+  description,
+  roomCount,
+  bathroomCount,
+  guestCount,
+  locationValue,
+}: Props) => {
+  const { getByValue } = useCountries();
+
+  const coordinates = getByValue(locationValue)?.latlng;
+
+  return (
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-8">
+        <div className="text-xl font-semibold flex flex-row items-center gap-2 ">
+          <div>Hosted by {user.name}</div>
+          <Avatar src={user.image} />
+        </div>
+        <div className="flex flex-row items-center gap-4 font-light text-neutral-500 ">
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        />
+      )}
+      <hr />
+      <div className="text-lg font-light text-neutral-500">{description}</div>
+      <hr />
+      <Map center={coordinates} />
+    </div>
+  );
+};
+
+export default ListingInfo;
