ab6f4f30920d4cc20081b2649be6832202d18fd9
Eduardo26 <jguevaral@uni.pe>
2023-05-25 18:09:41 -0500

    fix deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Listing } from "@prisma/client";
 import getListings, { IListingsParams } from "./actions/getListings";
 import Container from "./components/Container";
